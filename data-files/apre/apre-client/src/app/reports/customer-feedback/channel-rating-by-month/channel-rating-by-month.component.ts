a054afd72befdcdca501d118c01c60766b12a1db
Dustin Craven <godelvatai@gmail.com>
2026-06-27 04:03:17 -0400

    Week4-Minor: update channel rating by month component input
    Updated the input field for the channel rating by month component to include the red asterisk that marks the field as required.

--- data-files/apre/apre-client/src/app/reports/customer-feedback/channel-rating-by-month/channel-rating-by-month.component.ts
+++ data-files/apre/apre-client/src/app/reports/customer-feedback/channel-rating-by-month/channel-rating-by-month.component.ts
@@ -20,7 +20,7 @@ import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
         }
 
         <div class="form__group">
-          <label class="label" for="month">Month</label>
+          <label class="label" for="month">Month<span class="required">*</span></label>
           <select class="select" formControlName="month" id="month" name="month">
             @for(month of months; track month) {
               <option value="{{ month.value }}">{{ month.name }}</option>
